b7d79694a9b6995d57da27251167b6579d551629
Ben Math <benjamin.aaron.math@gmail.com>
2022-11-24 00:29:40 -0500

    fixed contact form

--- react-contact-form/src/server.js
+++ react-contact-form/src/server.js
@@ -37,7 +37,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "ben@benmath.io",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
